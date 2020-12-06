a3fd16008c124093e3bd350d12df17e0dfc42c6b
t0ny33 <enerazvan33@gmail.com>
2020-12-06 16:47:25 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-7s5f26
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-7s5f26)
---
+++ src/app/products.ts
@@ -0,0 +1,24 @@
+export const products = [
+  {
+    name: 'Phone XL 799$',
+    price: 799,
+    description: 'A large phone with one of the best screens'
+  },
+  {
+    name: 'Phone Mini 699$',
+    price: 699,
+    description: 'A great phone with one of the best cameras'
+  },
+  {
+    name: 'Phone Standard 299$',
+    price: 299,
+    description: 'A good all-rounder phone suitable for every need'
+  }
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
